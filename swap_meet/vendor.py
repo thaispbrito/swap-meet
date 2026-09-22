9c4f7517745353389dd7a045af8772ca314bf92d
Thais Palma de Brito <britop.tech@gmail.com>
2026-09-22 13:24:47 -0700

    Complete Wave 1 by creating Vendor class and its methods, as well as finishing the test cases

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+
+    def __init__(self, inventory=None):
+
+        self.inventory = [] if inventory is None else inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+
+        if item not in self.inventory:
+            return None
+
+        self.inventory.remove(item)
+        return item
+        
+        
