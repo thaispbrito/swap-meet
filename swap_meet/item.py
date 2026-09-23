b0ba8aefb22b3e3fa93ad6fe7d8f96f0664c13e8
porselvi <porselvi8989@gmail.com>
2026-09-22 18:13:16 -0700

    complete wave 5 by implementing three additional classes

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,9 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         self.id = uuid.uuid4().int if id is None else id
+        self.condition = condition
 
     def get_category(self):
         return "Item"
@@ -10,3 +11,14 @@ class Item:
     def __str__(self):
 
         return f"An object of type Item with id {self.id}."
+
+    def condition_description(self):
+        descriptions = {
+            0 : "poor",
+            1 : "heavily used",
+            2 : "used",
+            3 : "gently used",
+            4 : "like new",
+            5 : "mint"
+        }
+        return descriptions[self.condition]
