2786316b6496a54621bf64c23943457eebcbadf6
Thais Palma de Brito <britop.tech@gmail.com>
2026-09-24 19:22:32 -0700

    Fix condition_description method and add test cases

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -12,16 +12,24 @@ class Item:
         return f"An object of type {self.get_category()} with id {self.id}."
 
     def condition_description(self):
-        descriptions = {
-            0 : "poor",
-            1 : "heavily used",
-            2 : "used",
-            3 : "gently used",
-            4 : "like new",
-            5 : "mint"
-        }
-        return descriptions[self.condition]
 
+        if not 0 <= self.condition <= 5:
+            raise ValueError("condition must be between 0 and 5")
+
+        if self.condition == 0 :
+            description = "disgusting"
+        elif self.condition < 2:
+            description = "heavily used"
+        elif self.condition < 3:
+            description = "used"
+        elif self.condition < 4:
+            description = "gently used"
+        elif self.condition < 5:
+            description = "like new"
+        elif self.condition == 5:
+            description = "brand new"
+
+        return description
 
 if __name__ == "__main__":
     test = Item()
